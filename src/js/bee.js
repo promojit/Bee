ad10241a7622b9f4c4545d659f94daf16c71ceb9
Promojit Koley <promojitsanu@gmail.com>
2018-05-20 21:59:41 +0530

    Ripple effect added
    ripple animation added for all click event available now

--- src/js/bee.js
+++ src/js/bee.js
@@ -12,3 +12,42 @@ $(document).on("click", ".sidebar-show-background", function() {
   $(".sidebar-show-background").remove();
   $(".sidebar").removeClass("sidebar-show");
 });
+
+// Ripple Effect
+
+$(
+  ".button, .menu-list-item a, .icon-button, .icon-button-large, .complex-button"
+).click(function(e) {
+  // Remove any old one
+  $(".ripple").remove();
+
+  // Setup
+  var posX = $(this).offset().left,
+    posY = $(this).offset().top,
+    objWidth = $(this).width(),
+    objHeight = $(this).height();
+
+  // Add the element
+  $(this).prepend("<span class='ripple'></span>");
+
+  // Make it round!
+  if (objWidth >= objHeight) {
+    objHeight = objWidth;
+  } else {
+    objWidth = objHeight;
+  }
+
+  // Get the center of the element
+  var x = e.pageX - posX - objWidth / 2;
+  var y = e.pageY - posY - objHeight / 2;
+
+  // Add the ripples CSS and start the animation
+  $(".ripple")
+    .css({
+      width: objWidth,
+      height: objHeight,
+      top: y + "px",
+      left: x + "px"
+    })
+    .addClass("rippleEffect");
+});
